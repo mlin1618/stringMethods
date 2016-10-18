24bc4c853a8562b3dde9323017da243eb8c562b4
ml996 <ml996@pvsd.org>
2016-10-18 14:17:42 -0400

    ..

--- src/stringMethods.java
+++ src/stringMethods.java
@@ -1,6 +1,7 @@
 /**
  * Created by ml996 on 10/18/16.
  */
+import java.util.Scanner;
 public class stringMethods {
 
     public static void main(String[] args){
@@ -12,5 +13,16 @@ public class stringMethods {
         String s = "test1234test";
         String s2 = s.replace("test", "...");
         System.out.println(s2); //prints out ...1234...
+
+        //usage in simplistic program
+        //Program that does the Find+Replace function of Microsoft Word
+        System.out.println("Enter a string");
+        Scanner sc = new Scanner(System.in);
+        String original = sc.nextLine();
+        System.out.println("Enter the string you want to replace");
+        String replace1 = sc.nextLine();
+        System.out.println("Enter what you want to replace it with");
+        String new1 = sc.nextLine();
+        System.out.println(original.replace(replace1, new1));
     }
 }
